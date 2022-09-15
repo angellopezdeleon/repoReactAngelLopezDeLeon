be2a2a165730d1435ca1534bf83aced7d14f4d71
angel <angel.lopjar@gmail.com>
2022-09-15 23:16:17 +0200

    Desafío: Crea tu landing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
 import "./App.css";
-import NavBar from "./components/NavBar.jsx";
-import 'bootstrap/dist/css/bootstrap.css';
+import NavBar from "./components/NavBar/NavBar.jsx";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ItemListContainer greeting="Tienda de productos de coña"/>
     </div>
   );
 }
---
+++ src/components/NavBar/CartWidget.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function CartWidget() {
+		<img className="image-fluid" src="../../cart.png" alt="Carrito"/>
+}
+
+export default CartWidget;
